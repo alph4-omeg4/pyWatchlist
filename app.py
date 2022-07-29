e94dcd41f024092c931eb85b70df035d913e6c05
sanechek <alpha@bk.ru>
2022-07-29 19:01:32 +0500

    Implemented multy-user behavior Added search

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ menu = """Please select one of the following options:
 3) View all movies
 4) Watch a movie
 5) View watched movies.
-6) Exit.
+6) Add user to the app.
+7) Search for a movie.
+8) Exit.
 
 Your selection: """
 welcome = "Welcome to the watchlist app!"
@@ -28,32 +30,72 @@ def prompt_add_movie():
 
 def print_movie_list(heading: str, movies: list):
     print(f"--{heading} movies--")
-    for movie in movies:
-        movie_date = datetime.datetime.fromtimestamp(movie[1])
+    for _id, title, release_date in movies:
+        movie_date = datetime.datetime.fromtimestamp(release_date)
         human_date = movie_date.strftime('%d %b %Y')
-        print(f'{movie[0]} (on {human_date})')
+        print(f'{_id}: {title} (on {human_date})')
     print('----\n')
 
 
 def prompt_watch_movie():
-    title = input("Enter movie title you've watched: ")
+    username = input("Username: ")
+    movie_id = input("Enter movieID  you've watched: ")
     print('----\n')
-    database.watch_movie(title)
+    database.watch_movie(username, movie_id)
 
 
-while (user_input := input(menu)) != "6":
+def view_all_movies():
+    movies = database.get_movies()
+    print_movie_list('All', movies)
+
+
+def view_upcoming_movies():
+    movies = database.get_movies(upcoming=True)
+    print_movie_list('Upcoming', movies)
+
+
+def prompt_view_watched_movies():
+    username = input("Username: ")
+    movies = database.get_watched_movies(username)
+    if movies:
+        print_movie_list(f"{username}'s watched", movies)
+    else:
+        print(f"{username} hasn't watch any movies yet!\n")
+
+def prompt_add_user():
+    username = input("Username: ")
+    database.add_user(username)
+
+
+def prompt_search_movies():
+    pattern = input("Enter partial movie title you look for: ")
+    movies = database.search_movie(pattern)
+    if movies:
+        print_movie_list('Movies found', movies)
+    else:
+        print("No movies found\n")
+
+
+while (user_input := input(menu)) != "8":
     if user_input == "1":
+        '''Add new movie'''
         prompt_add_movie()
     elif user_input == "2":
-        movies = database.get_movies(upcoming=True)
-        print_movie_list('Upcoming', movies)
+        '''View upcoming movies'''
+        view_upcoming_movies()
     elif user_input == "3":
-        movies = database.get_movies()
-        print_movie_list('All', movies)
+        '''View all movies'''
+        view_all_movies()
     elif user_input == "4":
+        '''Watch a movie'''
         prompt_watch_movie()
     elif user_input == "5":
-        movies = database.get_watched_movies()
-        print_movie_list('Watched', movies)
+        '''View watched movies'''
+        prompt_view_watched_movies()
+    elif user_input == '6':
+        '''Reg new user'''
+        prompt_add_user()
+    elif user_input == '7':
+        prompt_search_movies()
     else:
         print("Invalid input, please try again!")
--- database.py
+++ database.py
@@ -3,30 +3,61 @@ from datetime import datetime
 
 
 CREATE_MOVIES_TABLE = """CREATE TABLE IF NOT EXISTS movies (
+    id INTEGER,
     title TEXT,
     release_timestamp REAL,
-    watched INTEGER
+    PRIMARY KEY (id)
 );"""
 
+CREATE_USERS_TABLE = """CREATE TABLE IF NOT EXISTS users (
+    username TEXT,
+    PRIMARY KEY (username)
+);"""
 
-INSERT_MOVIES = "INSERT INTO movies (title, release_timestamp, watched) VALUES (?, ?, 0);"
+CREATE_WATCHED_TABLE = """CREATE TABLE IF NOT EXISTS watched(
+    movie_id INTEGER,
+    user_username TEXT,
+    FOREIGN KEY (user_username) REFERENCES users(username),
+    FOREIGN KEY (movie_id) REFERENCES movies(id)
+);"""
+
+INSERT_MOVIES = "INSERT INTO movies (title, release_timestamp) VALUES (?, ?);"
+DELETE_MOVIE = "DELETE FROM movies WHERE title = ?"
+SEARCH_MOVIE = "SELECT * FROM movies WHERE title LIKE ?;"
+
 SELECT_ALL_MOVIES = "SELECT * FROM movies"
 SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
-SELECT_WATCHED_MOVIES = "SELECT * FROM movies where watched = 1;"
-SET_MOVIE_WATCHED = "UPDATE movies SET watched = 1 WHERE title = ?"
 
+INSERT_WATCHED_MOVIE = "INSERT INTO watched (user_username, movie_id) VALUES (?, ?);"
+SELECT_WATCHED_MOVIES = """SELECT movies.* FROM movies 
+    JOIN watched ON movies.id = watched.movie_id 
+    JOIN users ON users.username = watched.user_username 
+    WHERE users.username = ?;"""
+SET_MOVIE_WATCHED = "INSERT INTO watched (user_username, movie_id) VALUES (?, ?);"
+
+INSERT_USER = "INSERT INTO users (username) VALUES (?);"
+
+CREATE_RELEASEDATE_INDEX = "CREATE INDEX IF NOT EXISTS idx_movies_release ON movies(release_timestamp);"
 connection = sqlite3.connect("data.db")
 
 
 def create_tables():
     with connection:
         connection.execute(CREATE_MOVIES_TABLE)
+        connection.execute(CREATE_USERS_TABLE)
+        connection.execute(CREATE_WATCHED_TABLE)
+        connection.execute(CREATE_RELEASEDATE_INDEX)
 
-
-def add_movie(title: str, release_timestamp: float):
+def add_movie(title: str, release_timestamp):
     with connection:
         connection.execute(INSERT_MOVIES, (title, release_timestamp))
 
+
+def search_movie(pattern):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SEARCH_MOVIE, (f"%{pattern}%", ))
+        return cursor.fetchall()
 
 def get_movies(upcoming=False):
     with connection:
@@ -39,13 +70,18 @@ def get_movies(upcoming=False):
         return cursor.fetchall()
 
 
-def watch_movie(title: str):
+def watch_movie(username: str, movie_id: str):
     with connection:
-        connection.execute(SET_MOVIE_WATCHED, (title, ))
+        connection.execute(INSERT_WATCHED_MOVIE, (username, movie_id))
 
 
-def get_watched_movies():
+def get_watched_movies(username: str):
     with connection:
         cursor = connection.cursor()
-        cursor.execute(SELECT_WATCHED_MOVIES)
+        cursor.execute(SELECT_WATCHED_MOVIES, (username, ))
         return cursor.fetchall()
+
+
+def add_user(username: str):
+    with connection:
+        connection.execute(INSERT_USER, (username, ))
